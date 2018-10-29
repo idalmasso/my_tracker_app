2c96110a1298f986a237432d79bbe68c7dc89072
unknown <ivano.dalmasso@gmail.com>
2018-10-29 15:33:20 +0100

    Added favicon

--- tracker_app.py
+++ tracker_app.py
@@ -2,8 +2,9 @@ from app import mongo, create_app
 from app.usermodel import User
 from app.tracker import Tracker
 from app.projectmodel import TrkProject
-from flask import session
+from flask import session,send_from_directory
 from flask_login import login_required
+import os
 
 app = create_app()
 
@@ -12,6 +13,7 @@ def add_session_project_processor():
     project_choices=[('ALL','')]
     project_choices+=( [(p.id,p.name) for p in TrkProject.get_all()])
     val=''
+    print(app.root_path)
     if 'sessionproject' in session:
         val=session['sessionproject']
     return {'project_choices':project_choices, 'sessionproject':val}
@@ -19,9 +21,16 @@ def add_session_project_processor():
 @login_required
 @app.route('/set_session_project/<id>', methods=['POST'])
 def set_session_project(id):
+    
     session['sessionproject']=id
     return ''
 
 @app.shell_context_processor
 def make_shell_context():
     return {'mongo': mongo, 'User': User}
+
+@app.route('/favicon.ico')
+def favicon():
+    print(app.root_path)
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
